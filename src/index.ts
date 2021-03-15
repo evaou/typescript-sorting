4349590ca8eb58380faa4a2145d247e7c54b0a67
evaou <speeder528@gmail.com>
2021-03-16 07:46:19 +0800

    add interface for Sorter, and characters collection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([10000, 3, -5, 0]);
-const sort = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sort = new Sorter(numbersCollection);
+// sort.sort();
+// console.log(numbersCollection.data);
+
+const characterCollection = new CharactersCollection('Xaayb');
+const sort = new Sorter(characterCollection);
 sort.sort();
-console.log(numbersCollection.data);
+console.log(characterCollection.data);
