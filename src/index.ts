8026124bcd0b71f87c3a605fda5bf216852bdcc4
evaou <speeder528@gmail.com>
2021-03-16 06:57:39 +0800

    refactor out data

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
-
-const sort = new Sorter([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([10000, 3, -5, 0]);
+const sort = new Sorter(numbersCollection);
 sort.sort();
-console.log(sort.collection);
+console.log(numbersCollection.data);
