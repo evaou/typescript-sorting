83767fca41e019d5f179201bf8097523d8a925ad
evaou <speeder528@gmail.com>
2021-03-16 08:22:27 +0800

    add llinked list

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 // const sort = new Sorter(numbersCollection);
 // sort.sort();
 // console.log(numbersCollection.data);
 
-const characterCollection = new CharactersCollection('Xaayb');
-const sort = new Sorter(characterCollection);
+// const characterCollection = new CharactersCollection('Xaayb');
+// const sort = new Sorter(characterCollection);
+// sort.sort();
+// console.log(characterCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sort = new Sorter(linkedList);
 sort.sort();
-console.log(characterCollection.data);
+linkedList.print();
